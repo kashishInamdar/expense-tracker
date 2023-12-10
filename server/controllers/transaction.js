0fea9a96265c143ce9277ebc039a77dffb63570d
kashishInamdar <kashishinamdar2004@gmail.com>
2023-12-10 19:56:20 +0530

    crate delete user transaction by Id api

--- server/controllers/transaction.js
+++ server/controllers/transaction.js
@@ -1,8 +1,8 @@
 import Transaction from "../models/transaction.js";
 import { responder } from "../util.js";
 
-const PostApiTransaction =  async (req , res)=>{
-    const {amount , type , catagory , description , user } = req.body ;
+const PostApiTransaction = async (req, res) => {
+    const { amount, type, catagory, description, user } = req.body;
 
     const transaction = new Transaction({
         user,
@@ -11,39 +11,39 @@ const PostApiTransaction =  async (req , res)=>{
         catagory,
         description,
     });
-    try{
+    try {
         const respons = await transaction.save()
 
-        responder({ res , success: true , data : respons , message: "transaction add successfuly" })
+        responder({ res, success: true, data: respons, message: "transaction add successfuly" })
 
         // res.json({
         //     success : true,
         //     data : respons ,
         //     message : "transaction add successfuly"
         // })
-    }catch(err){
+    } catch (err) {
 
-        responder({message : err.message})
+        responder({ message: err.message })
         // res.json({
         //     success : false,
         //     message : err.message
         // })
 
     }
-    
+
 }
 
-const GetApiTransactions = async (req , res)=>{
-    try{
-        const transactions = await Transaction.find(); 
+const GetApiTransactions = async (req, res) => {
+    try {
+        const transactions = await Transaction.find();
 
-        responder({ res, success:true , data: transactions , message : "Successfuly fatch all Transaction" })
+        responder({ res, success: true, data: transactions, message: "Successfuly fatch all Transaction" })
 
-    }catch(err){
+    } catch (err) {
 
-        responder({message : err.message})
+        responder({ message: err.message })
     }
-    
+
 }
 
 const GetApiTransactionByUserId = async (req, res) => {
@@ -69,4 +69,23 @@ const GetApiTransactionByUserId = async (req, res) => {
     }
 }
 
-export {PostApiTransaction , GetApiTransactions , GetApiTransactionByUserId} ;
+const DeleteApiTransactionById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const deletTran = await Transaction.deleteOne({ _id: id })
+
+        res.json({
+            success: true,
+            data: deletTran,
+            massage: "Delete Transaction"
+        })
+    }catch(err){
+        res.json({
+            success : false,
+            massage : err.massage
+        })
+    }
+}
+
+export { PostApiTransaction, GetApiTransactions, GetApiTransactionByUserId, DeleteApiTransactionById };
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 dotenv.config();
 
-import { PostApiTransaction , GetApiTransactions , GetApiTransactionByUserId } from "./controllers/transaction.js";
+import { PostApiTransaction , GetApiTransactions , GetApiTransactionByUserId , DeleteApiTransactionById} from "./controllers/transaction.js";
 import { responder } from "./util.js";
 import User from "./models/user.js";
 import { PostApiV1Signup , GetApiV1Users  } from "./controllers/user.js";
@@ -31,7 +31,7 @@ app.get('/api/health' , async (req ,res)=>{
 app.post('/api/transaction' , PostApiTransaction)
 app.get("/api/transactions" , GetApiTransactions )
 app.get('/api/transactions/users/:id',GetApiTransactionByUserId)
-
+app.delete("/api/transactions/:id", DeleteApiTransactionById)
 
 // ------------ User ------------
 

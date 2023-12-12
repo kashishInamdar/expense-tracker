5c7a005cee8f09ea645ff1a70879cf02709edea7
kashishInamdar <kashishinamdar2004@gmail.com>
2023-12-12 22:00:29 +0530

    delete intigrate

--- client/src/component/Transactions/Transactions.js
+++ client/src/component/Transactions/Transactions.js
@@ -1,6 +1,19 @@
 import "./Transactions.css"
+import delet from "./delete.png"
+import edit from "./pencil.png"
+import axios from "axios"
+import showToast from 'crunchy-toast';
 
-const Transactions = ({ amount, type, index, catagory, description }) => {
+const deleteUserTransaction = async (id , loadTransaction) => {
+    const response = await axios.delete(`/api/transactions/${id}`);
+
+    if (response?.data?.success) {
+      showToast(response?.data?.message, 'success', '3000');
+      loadTransaction();
+    }
+  }
+
+const Transactions = ({ amount, type, index, catagory, description , id , loadTransaction }) => {
     return (
         <div className='transaction-box' key={index}>
             <span className={` ${type === "credit" ? "credit" : "debit"}  bold`} >
@@ -21,6 +34,12 @@ const Transactions = ({ amount, type, index, catagory, description }) => {
             </span>
             < hr />
             {description}
+            <img src={edit} alt="update" className="edit-btn" />
+            <img src={delet} alt="delete" className="delet-btn"
+            onClick={()=>{
+                deleteUserTransaction(id , loadTransaction)
+            }}
+            />
 
         </div>
     )
